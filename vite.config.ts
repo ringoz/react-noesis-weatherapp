ebe7e6b577f2e2969e3a08bc86ece176ca090c9f
Vladimir Davidovich <thy.ringo@gmail.com>
2022-08-31 11:15:32 +0300

    get rid of vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
@@ -22,14 +22,5 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@ringozz/*/build/*.wasm',
-          dest: '.',
-          preserveTimestamps: true,
-        },
-      ],
-    }),
   ],
 });
