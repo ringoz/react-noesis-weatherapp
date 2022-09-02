269440ffc15364d94ce75a502abc3a3766a7f3f6
Vladimir Davidovich <thy.ringo@gmail.com>
2022-09-02 21:55:45 +0300

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -18,5 +18,3 @@ export const App = () => {
     </ErrorBoundary>
   );
 };
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import { renderGUI } from '@ringozz/react-noesis';
-import App from './App';
+import { App } from './App';
 
 export default renderGUI(<App />, document.querySelector('canvas')!);
