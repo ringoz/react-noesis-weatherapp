fa0743e19088f8661afa3aeb2bd865bca5bfce00
Vladimir Davidovich <thy.ringo@gmail.com>
2022-09-06 14:52:47 +0300

    WeatherKit fetcher

--- README.md
+++ README.md
@@ -6,21 +6,15 @@
 
 Based on [React-WeatherApp](https://github.com/gheorghedarle/React-WeatherApp) by @gheorghedarle
 
-**Weather App** is a simple weather app developed in typescript using React and NoesisGUI. The app allows you to see the weather from your current location or another location around the globe using **OpenWeather Api** and **Google Maps Api**. Using **One Call API** the app displays the current weather, 24 hours and 6 days forecast. You can change the unit system from metric to imperial. The app is available in **light** and **dark mode**.
+**Weather App** is a simple weather app developed in typescript using React and NoesisGUI. The app allows you to see the weather from your current location or another location around the globe using **WeatherKit REST API**.
 
 ## Setup
 
 Create a file called **.env** in the root of the React-WeatherApp project. Add the following code in the file.
 
 ```
-VITE_APP_OPENWEATHER_API_BASEURL = "https://api.openweathermap.org/data/2.5/onecall"
-VITE_APP_OPENWEATHER_API_KEY = "YOUR_OPENWEATHERAPI_KEY"
-VITE_APP_GEOLOCATION_GEOCODE_BASEURL = "https://maps.googleapis.com/maps/api/geocode/json"
-VITE_APP_GEOLOCATION_API_KEY = "YOUR_GOOGLEMAPSAPI_KEY"
+VITE_APP_WEATHERKIT_BASEURL = "https://weatherkit.apple.com"
+VITE_APP_WEATHERKIT_TOKEN = "YOUR_WEATHERKIT_TOKEN"
 ```
-
-The app is using **One Call API** from OpenWeather API. To start the project you need an **account** and **OpenWeather API Key**. You can signup and get the key from [here](https://openweathermap.org/api).
-
-The app is using **Geocoding** and **Reverse Geocoding** from Google Maps API. To start the project you need a **google account** and **Google Maps API Key**. You can get the key from [here](https://developers.google.com/maps/documentation/geocoding/overview).
 
 To use real data, change the flag **useMockData** to false from **Container.tsx**.
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -10,15 +10,14 @@ import {
   EmptyHourlyWeatherModel,
   HourlyWeatherModel,
 } from "../models";
+import { Fetcher } from "openapi-typescript-fetch";
+import { components, paths } from "../models/WeatherKit";
 
 export const useWeather = (
   locationName: string,
   unit: string,
   useMockData: boolean
 ) => {
-  const baseUrl = import.meta.env.VITE_APP_OPENWEATHER_API_BASEURL;
-  const apiKey = import.meta.env.VITE_APP_OPENWEATHER_API_KEY;
-
   const { location } = useLocation(locationName, useMockData);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -35,14 +34,30 @@ export const useWeather = (
   useEffect(() => {
     setIsLoading(true);
     if (location) {
-      const url = useMockData
-        ? `assets/mock-data/weather_${unit}.json`
-        : `${baseUrl}?lat=${location.position.latitude}&lon=${location.position.longitude}&units=${unit}&exclude=minutely,alerts&appid=${apiKey}`;
-      fetch(url).then((response) => response.json())
+      const fetcher = Fetcher.for<paths>();
+      fetcher.configure(useMockData ? {
+        baseUrl: "assets/mock-data/weather.json?"
+      } : {
+        baseUrl: import.meta.env.VITE_APP_WEATHERKIT_BASEURL,
+        init: {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_APP_WEATHERKIT_TOKEN}`
+          }
+        }
+      });
+
+      const fetchWeather = fetcher.path("/api/v1/weather/{language}/{latitude}/{longitude}").method('get').create();
+      fetchWeather({
+        dataSets: ["currentWeather", "forecastHourly", "forecastDaily"],
+        language: navigator.language,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        latitude: location.position.latitude,
+        longitude: location.position.longitude
+      }).then((response) => response.data)
         .then((data) => {
-          setCurrent(data.current);
-          setHourly(data.hourly);
-          setDaily(data.daily);
+          setCurrent(data.currentWeather!);
+          setHourly(data.forecastHourly!);
+          setDaily(data.forecastDaily!);
         })
         .catch((error) => {
           handleError(error);
@@ -51,69 +66,69 @@ export const useWeather = (
           setTimeout(() => setIsLoading(false), 100);
         });
     }
-  }, [location, unit, useMockData, baseUrl, apiKey, handleError]);
+  }, [location, unit, useMockData, handleError]);
 
-  const setCurrent = (data: any) => {
+  const setCurrent = (data: components["schemas"]["CurrentWeather"]) => {
     setCurrentWeather({
-      dt: data.dt,
+      dt: Date.parse(data.asOf) / 1000,
       weather: {
-        icon: data.weather[0].icon,
-        description: data.weather[0].description,
+        icon: "01d",
+        description: data.conditionCode,
       },
-      temp: data.temp,
-      feels_like: data.feels_like,
+      temp: data.temperature,
+      feels_like: data.temperatureApparent,
       details: {
         rain: 0,
         visibility: data.visibility / 1000,
-        humidity: data.humidity,
+        humidity: Math.round(data.humidity * 100),
         pressure: data.pressure,
-        wind_speed: data.wind_speed,
+        wind_speed: data.windSpeed,
       },
     });
   };
 
-  const setHourly = (data: any) => {
+  const setHourly = (data: components["schemas"]["HourlyForecast"]) => {
     let hourly: CurrentWeatherModel[] = [];
-    data.slice(0, 24).forEach((item: any) => {
+    data.hours.forEach((item) => {
       hourly.push({
-        dt: item.dt,
+        dt: Date.parse(item.forecastStart!) / 1000,
         weather: {
-          icon: item.weather[0].icon,
-          description: item.weather[0].description,
+          icon: "01d",
+          description: item.conditionCode,
         },
-        temp: item.temp,
-        feels_like: item.feels_like,
+        temp: item.temperature,
+        feels_like: item.temperatureApparent,
         details: {
-          rain: item.pop * 100,
+          rain: Math.round(item.precipitationChance * 100),
           visibility: item.visibility / 1000,
-          humidity: item.humidity,
+          humidity: Math.round(item.humidity * 100),
           pressure: item.pressure,
-          wind_speed: item.wind_speed,
+          wind_speed: item.windSpeed,
         },
       });
     });
     setHourlyWeather({ hourly: hourly });
   };
 
-  const setDaily = (data: any) => {
+  const setDaily = (data: components["schemas"]["DailyForecast"]) => {
     let daily: DailyWeatherDetailsModel[] = [];
-    data.slice(1).forEach((item: any) => {
+    data.days.forEach((item) => {
       daily.push({
-        dt: item.dt,
-        clouds: item.clouds,
-        humidity: item.humidity,
-        pressure: item.pressure,
-        sunrise: item.sunrise,
-        sunset: item.sunset,
-        minTemp: item.temp.min,
-        maxTemp: item.temp.max,
-        uvi: item.uvi,
+        dt: Date.parse(item.forecastStart!) / 1000,
+        clouds: Math.round(item.daytimeForecast!.cloudCover * 100),
+        humidity: Math.round(item.daytimeForecast!.humidity * 100),
+        pressure: 0,
+        sunrise:  Date.parse(item.sunrise!) / 1000,
+        sunset: Date.parse(item.sunset!) / 1000,
+        minTemp: item.temperatureMin,
+        maxTemp: item.temperatureMax,
+        uvi: item.maxUvIndex,
         weather: {
-          icon: item.weather[0].icon,
-          description: item.weather[0].description,
+          icon: "01d",
+          description: item.conditionCode,
         },
-        wind_speed: item.wind_speed,
-        rain: item.pop * 100,
+        wind_speed: item.daytimeForecast!.windSpeed,
+        rain: Math.round(item.precipitationChance * 100),
       });
     });
     setDailyWeather({ daily: daily });
